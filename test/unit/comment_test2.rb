a480d2807c8d83588ada0f5a6f19bf342d53447f
G2iX <g2ix@g2ix-laptop.(none)>
2010-05-07 15:49:40 +0800

    database.yml

---
+++ test/unit/comment_test2.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class CommentTestDeletion < ActiveSupport::TestCase
+	def setup
+		@author = authors(:one)
+		@author2 = authors(:two)
+		@article = articles(:one)
+		@comment = comments(:one)
+	end
+	
+	test "should be able to delete comment using author_id" do
+		@article.author = @author
+		@article.save
+		@article.comments << @comment
+		assert_difference '@article.comments(true).count', -1 do
+			@comment.delete_by_author(@author.id)
+		end	
+	end
+	
+	test "should not be able to delete comment using author_id" do
+		@article.author = @author
+		@article.save
+		@article.comments << @comment
+		assert_no_difference '@article.comments.count' do
+			@comment.delete_by_author(@author2.id)
+		end
+	end
+		
+	test "should be able to delete comment" do
+		@article.author = @author
+		@article.save
+		@article.comments << @comment
+		assert_difference '@article.comments(true).count', -1 do
+			@comment.delete_by_author(@author)
+		end
+	end
+	
+	test "should not be able to delete comment" do
+		@article.author = @author
+		@article.save
+		@article.comments << @comment
+		assert_no_difference '@article.comments.count' do
+			@comment.delete_by_author(@author2)
+		end
+	end
+end
