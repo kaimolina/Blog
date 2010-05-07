744c24e80d1aeccccba60595b946c672dd77cb37
G2iX <g2ix@g2ix-laptop.(none)>
2010-05-07 17:28:19 +0800

    login field

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ActiveRecord::Base
   has_many :articles
   validates_presence_of [:first_name, :last_name], :message => "First Name and Last Name required."
   validates_uniqueness_of [:email, :login]
-  acts_as_authentic
+  acts_as_authentic {|c| c.login_field = :login}
   def full_name
     "#{last_name}, #{first_name}"
   end
